27fbdcc0e780dfa6b0b25f8f92033ed64e9e4757
Boykatt11 <moeedmustafa1999@gmail.com>
2025-05-01 07:30:39 +0000

    third commit

--- main.cpp
+++ main.cpp
@@ -17,11 +17,13 @@ int screen_y = 900;
 // prototypes 
 void player_gravity(char** lvl, float& offset_y, float& velocityY, bool& onGround, float& gravity, float& terminal_Velocity, int& hit_box_factor_x, int& hit_box_factor_y, float& player_x, float& player_y, const int cell_size, int& Pheight, int& Pwidth);
 
-void draw_player(RenderWindow& window, Sprite& LstillSprite, float player_x, float player_y);
+void draw_player(RenderWindow& window, Sprite& RstillSprite, Sprite& LstillSprite, float player_x, float player_y, Sprite& leftjog, Sprite& rightjog, bool& switche, bool& twitche, bool& bruh, bool& truh, Sprite& leftup, Sprite& rightup);
 
 void display_level(RenderWindow& window, const int height, const int width, char** lvl, Sprite& wallSprite1, const int cell_size, Sprite& brickSp1, Sprite& brickSp2, Sprite& brickSp3, Sprite spikeSp);
 
-void movePlayer(float& player_x, float& player_y, float jumpStrength, Clock& delayjump, bool& onGround, float& velocityY, char& bottom_left_down, char& bottom_right_down, char** lvl, char& top_mid_up);
+void movePlayer(float& player_x, float& player_y, float jumpStrength, Clock& delayjump, bool& onGround, float& velocityY, char& bottom_left_down, char& bottom_right_down, char** lvl, char& top_mid_up, bool& switche, bool& twitche, const int cell_size, const int height, const int width, bool& bruh, bool& truh);
+
+void animation(Sprite& LstillSprite, Sprite& leftjog, Texture& leftjog1, Texture& leftjog2, Texture& leftjog3, Texture& leftjog4, Texture& leftjog5, Texture& leftjog6, Texture& leftjog7, Texture& leftjog8, Texture& leftjog9, Texture& leftjog10, Sprite& rightjog, Texture& rightjog1, Texture& rightjog2, Texture& rightjog3, Texture& rightjog4, Texture& rightjog5, Texture& rightjog6, Texture& rightjog7, Texture& rightjog8, Texture& rightjog9, Texture& rightjog10, float scale_x, float scale_y, bool& switche, bool& twitche, bool toggle[], Sprite& leftup, Texture& leftup1, Texture& leftup2, Texture& leftup3, Texture& leftup4, Texture& leftup5, Texture& leftup6, Texture& leftup7, Texture& leftup8, Sprite& rightup, Texture& rightup1, Texture& rightup2, Texture& rightup3, Texture& rightup4, Texture& rightup5, Texture& rightup6, Texture& rightup7, Texture& rightup8, bool boggle[], bool& bruh);
 
 void moveView(View& view, float player_x, float player_y, FloatRect& cameraview);
 int main()
@@ -345,8 +347,117 @@ int main()
 	float jumpStrength = -20; // Initial jump velocity
 	float gravity = 1;  // Gravity acceleration
 
+
+	//SPrites for sonic!!
 	Texture LstillTex;
 	Sprite LstillSprite;
+    Texture RstillTex;
+Sprite RstillSprite;
+bool bruh;
+bool truh;
+
+
+Sprite leftjog;
+Texture leftjog1;
+Texture leftjog2;
+Texture leftjog3;
+Texture leftjog4;
+Texture leftjog5;
+Texture leftjog6;
+Texture leftjog7;
+Texture leftjog8;
+Texture leftjog9;
+Texture leftjog10;
+
+Sprite rightjog;
+Texture rightjog1;
+Texture rightjog2;
+Texture rightjog3;
+Texture rightjog4;
+Texture rightjog5;
+Texture rightjog6;
+Texture rightjog7;
+Texture rightjog8;
+Texture rightjog9;
+Texture rightjog10;
+
+Sprite leftup;
+Texture leftup1;
+Texture leftup2;
+Texture leftup3;
+Texture leftup4;
+Texture leftup5;
+Texture leftup6;
+Texture leftup7;
+Texture leftup8;
+
+Sprite rightup;
+Texture rightup1;
+Texture rightup2;
+Texture rightup3;
+Texture rightup4;
+Texture rightup5;
+Texture rightup6;
+Texture rightup7;
+Texture rightup8;
+
+LstillTex.loadFromFile("Data/0left_still.png");
+LstillSprite.setTexture(LstillTex);
+LstillSprite.setScale(scale_x, scale_y);
+
+RstillTex.loadFromFile("Data/0right_still.png");
+RstillSprite.setTexture(RstillTex);
+RstillSprite.setScale(scale_x, scale_y);
+
+bool switche, twitche;
+leftjog1.loadFromFile("Data/leftjog1.png");
+leftjog2.loadFromFile("Data/leftjog2.png");
+leftjog3.loadFromFile("Data/leftjog3.png");
+leftjog4.loadFromFile("Data/leftjog4.png");
+leftjog5.loadFromFile("Data/leftjog5.png");
+leftjog6.loadFromFile("Data/leftjog6.png");
+leftjog7.loadFromFile("Data/leftjog7.png");
+leftjog8.loadFromFile("Data/leftjog8.png");
+leftjog9.loadFromFile("Data/leftjog9.png");
+leftjog10.loadFromFile("Data/leftjog10.png");
+
+rightjog1.loadFromFile("Data/rightjog1.png");
+rightjog2.loadFromFile("Data/rightjog2.png");
+rightjog3.loadFromFile("Data/rightjog3.png");
+rightjog4.loadFromFile("Data/rightjog4.png");
+rightjog5.loadFromFile("Data/rightjog5.png");
+rightjog6.loadFromFile("Data/rightjog6.png");
+rightjog7.loadFromFile("Data/rightjog7.png");
+rightjog8.loadFromFile("Data/rightjog8.png");
+rightjog9.loadFromFile("Data/rightjog9.png");
+rightjog10.loadFromFile("Data/rightjog10.png");
+
+leftup1.loadFromFile("Data/leftup1.png");
+leftup2.loadFromFile("Data/leftup2.png");
+leftup3.loadFromFile("Data/leftup3.png");
+leftup4.loadFromFile("Data/leftup4.png");
+leftup5.loadFromFile("Data/leftup5.png");
+leftup6.loadFromFile("Data/leftup6.png");
+leftup7.loadFromFile("Data/leftup7.png");
+leftup8.loadFromFile("Data/leftup8.png");
+
+rightup1.loadFromFile("Data/rightup1.png");
+rightup2.loadFromFile("Data/rightup2.png");
+rightup3.loadFromFile("Data/rightup3.png");
+rightup4.loadFromFile("Data/rightup4.png");
+rightup5.loadFromFile("Data/rightup5.png");
+rightup6.loadFromFile("Data/rightup6.png");
+rightup7.loadFromFile("Data/rightup7.png");
+rightup8.loadFromFile("Data/rightup8.png");
+
+bool toggle[10] = { 0 };
+int i = 0;
+
+bool boggle[8] = { 0 };
+int j = 0;
+    
+
+
 
 	bool onGround = false;
 
@@ -424,10 +535,11 @@ int main()
 
 		window.clear();
 
-		display_level(window, height, width, lvl, wallSprite1, cell_size, brickSp1, brickSp2, brickSp3, spikeSp);
 		moveView(view, player_x, player_y, cameraview);
-		draw_player(window, LstillSprite, player_x, player_y);
-		movePlayer(player_x, player_y, jumpStrength, delayjump, onGround, velocityY, bottom_left_down, bottom_right_down, lvl, top_mid_up);
+		animation(LstillSprite, leftjog, leftjog1, leftjog2, leftjog3, leftjog4, leftjog5, leftjog6, leftjog7, leftjog8, leftjog9, leftjog10, rightjog, rightjog1, rightjog2, rightjog3, rightjog4, rightjog5, rightjog6, rightjog7, rightjog8, rightjog9, rightjog10, scale_x, scale_y, switche, twitche, toggle, leftup, leftup1, leftup2, leftup3, leftup4, leftup5, leftup6, leftup7, leftup8, rightup, rightup1, rightup2, rightup3, rightup4, rightup5, rightup6, rightup7, rightup8, boggle, bruh);
+		draw_player(window, RstillSprite, LstillSprite, player_x, player_y, leftjog, rightjog, switche, twitche, bruh, truh, leftup, rightup);
+		movePlayer(player_x, player_y, jumpStrength, delayjump, onGround, velocityY, bottom_left_down, bottom_right_down, lvl, top_mid_up, switche, twitche, cell_size, height, width, bruh, truh);
+
 		mottobug.update();
 		mottobug.render(window);
 		window.setView(view);
@@ -494,10 +606,54 @@ void player_gravity(char** lvl, float& offset_y, float& velocityY, bool& onGroun
 	}
 
 }
-void draw_player(RenderWindow& window, Sprite& LstillSprite, float player_x, float player_y) {
-
-	LstillSprite.setPosition(player_x, player_y);
-	window.draw(LstillSprite);
+void draw_player(RenderWindow& window, Sprite& RstillSprite, Sprite& LstillSprite, float player_x, float player_y, Sprite& leftjog, Sprite& rightjog, bool& switche, bool& twitche, bool& bruh, bool& truh, Sprite& leftup, Sprite& rightup) 
+{
+	if (switche)
+	{
+		//cout << "X:" << player_x << "  Y:" << player_y << endl;
+		leftjog.setPosition(player_x, player_y);
+		window.draw(leftjog);
+		bruh = true;
+	}
+	else if (twitche)
+	{
+		cout << "X:" << player_x << "  Y:" << player_y << endl;
+		rightjog.setPosition(player_x, player_y);
+		window.draw(rightjog);
+	}
+	else
+	{
+		if (bruh)
+		{
+			//cout << "X:" << player_x << "  Y:" << player_y << endl;
+			if (truh)
+			{
+				cout << "UPPPPP"<<endl;
+				leftup.setPosition(player_x, player_y);
+				window.draw(leftup);
+			}
+			else
+			{
+				LstillSprite.setPosition(player_x, player_y);
+				window.draw(LstillSprite);
+			}
+			
+		}
+		else
+		{
+			if (truh)
+			{
+				cout << "UPPPPP" << endl;
+				rightup.setPosition(player_x, player_y);
+				window.draw(rightup);
+			}
+			else
+			{
+				RstillSprite.setPosition(player_x, player_y);
+				window.draw(RstillSprite);
+			}
+		}
+	}
 
 }
 void display_level(RenderWindow& window, const int height, const int width, char** lvl, Sprite& wallSprite1, const int cell_size, Sprite& brickSp1, Sprite& brickSp2, Sprite& brickSp3, Sprite spikeSp)
@@ -560,45 +716,293 @@ void display_level(RenderWindow& window, const int height, const int width, char
 		}
 	}
 }
-void movePlayer(float& player_x, float& player_y, float jumpStrength, Clock& delayjump, bool& onGround, float& velocityY, char& bottom_left_down, char& bottom_right_down, char** lvl, char& top_mid_up)
+
+void animation(Sprite& LstillSprite, Sprite& leftjog, Texture& leftjog1, Texture& leftjog2, Texture& leftjog3, Texture& leftjog4, Texture& leftjog5, Texture& leftjog6, Texture& leftjog7, Texture& leftjog8, Texture& leftjog9, Texture& leftjog10, Sprite& rightjog, Texture& rightjog1, Texture& rightjog2, Texture& rightjog3, Texture& rightjog4, Texture& rightjog5, Texture& rightjog6, Texture& rightjog7, Texture& rightjog8, Texture& rightjog9, Texture& rightjog10, float scale_x, float scale_y, bool& switche, bool& twitche, bool toggle[], Sprite& leftup, Texture& leftup1, Texture& leftup2, Texture& leftup3, Texture& leftup4, Texture& leftup5, Texture& leftup6, Texture& leftup7, Texture& leftup8, Sprite& rightup, Texture& rightup1, Texture& rightup2, Texture& rightup3, Texture& rightup4, Texture& rightup5, Texture& rightup6, Texture& rightup7, Texture& rightup8, bool boggle[], bool& bruh)
+{
+	Vector2f currentPos = leftjog.getPosition();
+	if (switche == true)
+	{
+			if (toggle[0] == true)
+			{
+				leftjog.setTexture(leftjog1);
+				leftjog.setScale(scale_x, scale_y);
+			}
+			else if (toggle[1] == true)
+			{
+				leftjog.setTexture(leftjog2);
+				leftjog.setScale(scale_x, scale_y);
+			}
+			else if (toggle[2] == true)
+			{
+				leftjog.setTexture(leftjog3);
+				leftjog.setScale(scale_x, scale_y);
+			}
+			else if (toggle[3] == true)
+			{
+				leftjog.setTexture(leftjog4);
+				leftjog.setScale(scale_x, scale_y);
+			}
+			else if (toggle[4] == true)
+			{
+				leftjog.setTexture(leftjog5);
+				leftjog.setScale(scale_x, scale_y);
+			}
+			else if (toggle[5] == true)
+			{
+				leftjog.setTexture(leftjog6);
+				leftjog.setScale(scale_x, scale_y);
+			}
+			else if (toggle[6] == true)
+			{
+				leftjog.setTexture(leftjog7);
+				leftjog.setScale(scale_x, scale_y);
+			}
+			else if (toggle[7] == true)
+			{
+				leftjog.setTexture(leftjog8);
+				leftjog.setScale(scale_x, scale_y);
+			}
+			else if (toggle[8] == true)
+			{
+				leftjog.setTexture(leftjog9);
+				leftjog.setScale(scale_x, scale_y);
+			}
+			else if (toggle[9] == true)
+			{
+				leftjog.setTexture(leftjog10);
+				leftjog.setScale(scale_x, scale_y);
+			}
+		leftjog.setPosition(currentPos);
+	}
+
+	else if (twitche == true)
+	{
+		Vector2f currentPos1 = rightjog.getPosition();
+		if (toggle[0] == true)
+		{
+			rightjog.setTexture(rightjog1);
+			rightjog.setScale(scale_x, scale_y);
+		}
+		else if (toggle[1] == true)
+		{
+			rightjog.setTexture(rightjog2);
+			rightjog.setScale(scale_x, scale_y);
+		}
+		else if (toggle[2] == true)
+		{
+			rightjog.setTexture(rightjog3);
+			rightjog.setScale(scale_x, scale_y);
+		}
+		else if (toggle[3] == true)
+		{
+			rightjog.setTexture(rightjog4);
+			rightjog.setScale(scale_x, scale_y);
+		}
+		else if (toggle[4] == true)
+		{
+			rightjog.setTexture(rightjog5);
+			rightjog.setScale(scale_x, scale_y);
+		}
+		else if (toggle[5] == true)
+		{
+			rightjog.setTexture(rightjog6);
+			rightjog.setScale(scale_x, scale_y);
+		}
+		else if (toggle[6] == true)
+		{
+			rightjog.setTexture(rightjog7);
+			rightjog.setScale(scale_x, scale_y);
+		}
+		else if (toggle[7] == true)
+		{
+			rightjog.setTexture(rightjog8);
+			rightjog.setScale(scale_x, scale_y);
+		}
+		else if (toggle[8] == true)
+		{
+			rightjog.setTexture(rightjog9);
+			rightjog.setScale(scale_x, scale_y);
+		}
+		else if (toggle[9] == true)
+		{
+			rightjog.setTexture(rightjog10);
+			rightjog.setScale(scale_x, scale_y);
+		}
+		rightjog.setPosition(currentPos1);
+	}
+	else
+	{
+
+	}
+	if (bruh)
+	{
+		Vector2f currentPos = leftup.getPosition();
+		if (boggle[0])
+		{
+			leftup.setTexture(leftup1);
+			leftup.setScale(scale_x, scale_y);
+		}
+		else if (boggle[1])
+		{
+			leftup.setTexture(leftup2);
+			leftup.setScale(scale_x, scale_y);
+		}
+		else if (boggle[2])
+		{
+			leftup.setTexture(leftup3);
+			leftup.setScale(scale_x, scale_y);
+		}
+		else if (boggle[3])
+		{
+			leftup.setTexture(leftup4);
+			leftup.setScale(scale_x, scale_y);
+		}
+		else if (boggle[4])
+		{
+			leftup.setTexture(leftup5);
+			leftup.setScale(scale_x, scale_y);
+		}
+		else if (boggle[5])
+		{
+			leftup.setTexture(leftup6);
+			leftup.setScale(scale_x, scale_y);
+		}
+		else if (boggle[6])
+		{
+			leftup.setTexture(leftup7);
+			leftup.setScale(scale_x, scale_y);
+		}
+		else if (boggle[7])
+		{
+			leftup.setTexture(leftup8);
+			leftup.setScale(scale_x, scale_y);
+		}
+		leftup.setPosition(currentPos);
+	}
+	else
+	{
+		Vector2f currentPos = rightup.getPosition();
+		if (boggle[0])
+		{
+			rightup.setTexture(rightup1);
+			rightup.setScale(scale_x, scale_y);
+		}
+		else if (boggle[1])
+		{
+			rightup.setTexture(rightup2);
+			rightup.setScale(scale_x, scale_y);
+		}
+		else if (boggle[2])
+		{
+			rightup.setTexture(rightup3);
+			rightup.setScale(scale_x, scale_y);
+		}
+		else if (boggle[3])
+		{
+			rightup.setTexture(rightup4);
+			rightup.setScale(scale_x, scale_y);
+		}
+		else if (boggle[4])
+		{
+			rightup.setTexture(rightup5);
+			rightup.setScale(scale_x, scale_y);
+		}
+		else if (boggle[5])
+		{
+			rightup.setTexture(rightup6);
+			rightup.setScale(scale_x, scale_y);
+		}
+		else if (boggle[6])
+		{
+			rightup.setTexture(rightup7);
+			rightup.setScale(scale_x, scale_y);
+		}
+		else if (boggle[7])
+		{
+			rightup.setTexture(rightup8);
+			rightup.setScale(scale_x, scale_y);
+		}
+		rightup.setPosition(currentPos);
+	}
+}
+
+void movePlayer(float& player_x, float& player_y, float jumpStrength, Clock& delayjump, bool& onGround, float& velocityY, char& bottom_left_down, char& bottom_right_down, char** lvl, char& top_mid_up, bool& switche, bool& twitche, const int cell_size, const int height, const int width, bool& bruh, bool& truh)
 {
 	Keyboard key;
+	// Calculate player's grid position (for collision checks)
+	int gridX = static_cast<int>(player_x) / cell_size;
+	int gridY = static_cast<int>(player_y) / cell_size;
 	if (key.isKeyPressed(key.Left))
 	{
-		player_x -= 25;
-		//switche = true;
+		if (gridX > 0 && lvl[gridY][gridX] != 'w')
+		{
+			player_x -= 8;
+			if (!onGround && key.isKeyPressed(key.Space))
+				truh = true;
+			else
+			{
+				truh = false;
+				twitche = false;
+				switche = true;
+			}
+		}
+		bruh = true;
 	}
 	else if (key.isKeyPressed(key.Right))
 	{
-		player_x += 25;
-		//twitche = true;
+		if (gridX < width - 1 && lvl[gridY][gridX+2] != 'w')
+		{
+			player_x += 8;
+			if (!onGround && key.isKeyPressed(key.Space))
+				truh = true;
+			else
+			{
+				truh = false;
+				twitche = true;
+				switche = false;
+			}
+		}
+		bruh = false;
+	}
+	else
+	{
+		twitche = false;
+		switche = false;
 	}
 	if (key.isKeyPressed(key.Space) && delayjump.getElapsedTime().asSeconds() > 0.3 && onGround)
 	{
+		cout << "UPPPPP DETECTION" << endl;
+		truh = true;
 		velocityY = +jumpStrength;
-
 		delayjump.restart();
 	}
+	/*if (!onGround && key.isKeyPressed(key.Space))
+		truh = true;
+	else
+		truh = false;*/
+	//if (key.isKeyPressed(key.Left)&&onGround&&(lvl[static_cast<int>(player_y)-20][static_cast<int>(player_x+20)]!='w'))
+
+
 	//if(bottom_right_down=='w')
 
 
-	/*if (key.isKeyPressed(key.Space) && delayjump.getElapsedTime().asSeconds() > 0.3)
-	{
+	//if (key.isKeyPressed(key.Space) && delayjump.getElapsedTime().asSeconds() > 0.3)
+	//{
 
-		Clock delayclock3;
-		cloc = true;
-		player_y += (jumpStrength);
-		cout << "here";
-		delayjump2.restart();
-		if (delayjump2.getElapsedTime().asSeconds() > 0.9)
-		{
-			delayjump.restart();
-			delayjump2.restart();
-			cout << "now here!";
-		}
-		delayjump.restart();
-	}
-	if (cloc)
+	//	/*Clock delayclock3;*/
+	//	cloc = true;
+	//	player_y += (jumpStrength);
+	//	cout << "here";
+	//	//delayjump2.restart();
+	//	if (delayjump2.getElapsedTime().asSeconds() > 0.9)
+	//	{
+	//		delayjump.restart();
+	//		delayjump2.restart();
+	//		cout << "now here!";
+	//	}
+	//	//delayjump.restart();
+	//}
+	/*if (cloc)
 	{
 		if (delayjump.getElapsedTime().asMicroseconds() > 1)
 		{
